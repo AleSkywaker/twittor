1010a9537f9d47596e302b995f8e904d0461510c
Alex Colombo <uricaine@hotmail.com>
2019-03-08 18:55:35 +0100

    creating serviceWorker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const STTIC_CACHE = "sttic-v1";
+const STATIC_CACHE = "static-v1";
 const DYNAMIC_CACHE = "dynamic-v1";
 const INMUTABLE_CACHE = "inmutable-v1";
 
@@ -21,3 +21,9 @@ const APP_SHELL_INMUTABLE = [
     "css/animate.css",
     "js/libs/jquery.js"
 ]
+
+self.addEventListener('install', e=>{
+    const cacheStatic = caches.open(STATIC_CACHE).then(cache =>{
+        
+    })
+})

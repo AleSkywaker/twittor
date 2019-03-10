e97f58baf6ecd2f13c5677b7a954b4732e1fccf2
Alex Colombo <uricaine@hotmail.com>
2019-03-10 11:27:49 +0100

    added sw utils

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -2,5 +2,9 @@
 
 //Guardar cache dinamico
 function actualizaCacheDinamico(dynamicCache, req, res){
-
+     if(res.ok){
+         caches.open(dymanicCache).then(cache=>{
+             cache.put(req, res.clone());
+         })
+     }
 }
